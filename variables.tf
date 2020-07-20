79b4a0fcfb1cfd35741052840493e60f7a48a3d5
Karim Benhamouche <kbenhamouche@kbenhamouch-a01.vmware.com>
2020-07-20 12:10:31 -0400

    ok

---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "nsx_server" {
+  description = "NSX-T Manager fqdn or ip"
+}
+
+variable "nsx_user" {
+  description = "NSX user"
+}
+
+variable "nsx_password" {
+  description = "NSX password"
+}
