27a9b3a8edb3a4659d7b864c5a97dcf32e4cdce3
Karim Benhamouche <kbenhamouche@kbenhamouch-a01.vmware.com>
2020-07-22 14:00:20 -0400

    ok

--- resources.tf
+++ resources.tf
@@ -1,15 +1,3 @@
-// Configure the NSXT provider
-provider "nsxt" {
-  host                     = "10.5.99.151"
-  username                 = var.nsx_username
-  password                 = var.nsx_password
-  allow_unverified_ssl     = true
-  max_retries              = 10
-  retry_min_delay          = 500
-  retry_max_delay          = 5000
-  retry_on_status_codes    = [429]
-}
-
 #
 # This part of the example shows creating Groups with dynamic membership
 # criteria
